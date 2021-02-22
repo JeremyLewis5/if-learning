1e43e3fe79c7924f9f688c34f960f9bf309d71fa
JeremyLewis5 <jeremy.lewis@codeimmersives.com>
2021-02-22 15:54:16 -0600

    new changes

--- main.js
+++ main.js
@@ -2,23 +2,44 @@
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
 function isItLong(str) {
-   return 
+ if ( str.length > 20) {
+     return "That's a long string!";
+   }
 }
 
 
-function isItMedium() {
-
+function isItMedium(str) {
+    if (str.length  >= 10 && str.length  <= 20) {
+      return "That's a regular sized string!";
+    }
 }
 
-function isItShort(){
-
+function isItShort(str) {
+  if (str.length < 10 ) {
+    return "That's a small string!";
+  } else (str.length > 10 ) 
+    {return "That's not a small string!"
+  }
 }
 
-function howLongIsMyString() {
-
+function howLongIsMyString(str) {
+    if (str.length > 20) {
+      return "That's a long string!" 
+    } else if (str.length >= 10) {
+      return "That's a regular sized string!" 
+    } else {
+      return "That's a small string!"
+    }
 }
 
-function instructorHeight() {
+function instructorHeight(str) {
+  if (str === 'Colin') {
+    return 62;
+  } else if (str === 'Mesuara') {
+    return 69;
+  } else {
+    return "I don't know that instructor!";
+  }
 
 }
 
